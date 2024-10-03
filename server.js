a61038dc842fb0e40d5c2a8ea24f31d88790a9bd
Rishab151 <143947980+Rish151@users.noreply.github.com>
2024-10-03 12:20:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,10 +42,4 @@ connectDb()
     );
   });
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/dist"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-  });
-}
+
