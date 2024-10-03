3a58afdc9d92b510f5f2add32c1892a52247f20c
Rishab151 <143947980+Rish151@users.noreply.github.com>
2024-10-03 14:44:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,26 @@ const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 // Use CORS origin from environment variable or fallback to localhost for development
+const allowedOrigins = [
+  "https://technicaltwist.netlify.app",
+  "http://localhost:5173",
+  // Add more origins if needed
+];
+
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error("Not allowed by CORS")); // Reject the request
+    }
+  },
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+
 
 app.use(express.json()); // To parse JSON bodies
 app.use("/api/auth", authRoute);
